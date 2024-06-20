d8359299faef64cf955a659d5d5fe207cc0fd888
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-06-20 13:41:13 +0530

    Login and Signup

---
+++ frontend/notes-app/src/components/ProfileInfo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProfileInfo = () => {
+  return (
+    <div className=''>
+        <div className="">HO</div>
+            <div>
+                <p className="text-sm font-medium">Gouresh</p> 
+            </div>
+        
+    </div>
+  )
+}
+
+export default ProfileInfo
