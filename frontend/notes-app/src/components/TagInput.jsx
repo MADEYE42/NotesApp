d64e294c7a0a5741aa2da7df4001e9af1e1edc94
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-06-21 20:30:28 +0530

    FrontEnd Done

--- frontend/notes-app/src/components/TagInput.jsx
+++ frontend/notes-app/src/components/TagInput.jsx
@@ -1,49 +1,67 @@
-import React, { useState } from 'react';
-import { MdAdd ,MdClose} from "react-icons/md";
+import React, { useState } from "react";
+import { MdAdd, MdClose } from "react-icons/md";
 
-const TagInput = (tags,setTags) => {
-    const [inputValue,setInputValue]=useState("");
-    const handleInputChange=(e)=>{
-        setInputValue(e.target.value);
+const TagInput = ({ tags, setTags }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addNewTag = () => {
+    if (inputValue.trim() !== "") {
+      setTags([...tags, inputValue.trim()]);
+      setInputValue("");
     }
-    const addNewTag=()=>{
-        if(inputValue.tri()!==""){
-            setTags([...tags,inputValue.trim()]);
-            setInputValue("");
-        }
-    };
-    const handleKeyDown = (e)=>{
-        if(e.key==="Enter"){
-            addNewTag();
-        }
-    };
-    const handleRemoveTag = (tagToRemove)=>{
-        setTags(tags.filter((tag)=>tag!==tagToRemove));
-    };
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      addNewTag();
+    }
+  };
+
+  const handleRemoveTag = (tagToRemove) => {
+    setTags(tags.filter((tag) => tag !== tagToRemove));
+  };
+
   return (
     <div>
-        {tags?.length>0 &&( <div className="flex items-center gap-2 flex-wrap mt-2 ">
-            {tags.map((tag,index)=>(
-            <span key={index} className=' '>#{tag}
-            <button onClick={()=>{handleRemoveTag(tag)}} ><MdClose/></button></span>
-        ))};
-        </div>)}
-
-
-        <div className="flex items-center gap-4 mt-3 ">
-            <input
-                type='text'
-                className='text-sm bg-transparent border px-3 py-2 rounded outline-none'
-                placeholder='Add Tags'
-                onChange={handleInputChange}
-                onKeyDown={handleKeyDown}
-            />
-            <button className="w-5 h-5 flex items-center justify-center rounded border border-blue-700 hover:bg-blue-700" onClick={()=>{addNewTag()}}>
-               <MdAdd className='text-2xl text-blue-700 hover:text-blue-100 '/>
-            </button>
+      {tags?.length > 0 && (
+        <div className="flex items-center gap-2 flex-wrap mt-2">
+          {tags.map((tag, index) => (
+            <span key={index} className="">
+              #{tag}
+              <button
+                onClick={() => {
+                  handleRemoveTag(tag);
+                }}
+              >
+                <MdClose />
+              </button>
+            </span>
+          ))}
         </div>
-    </div>
-  )
-}
+      )}
 
-export default TagInput
+      <div className="flex items-center gap-4 mt-3">
+        <input
+          type="text"
+          className="text-sm bg-transparent border px-3 py-2 rounded outline-none"
+          placeholder="Add Tags"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyDown={handleKeyDown}
+        />
+        <button
+          className="w-5 h-5 flex items-center justify-center rounded border border-blue-700 hover:bg-blue-700"
+          onClick={addNewTag}
+        >
+          <MdAdd className="text-2xl text-blue-700 hover:text-blue-100" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TagInput;
